140d9d60ab4bb3e2ed2040bc4644f28c11147943
Dominik Kuhnke <dominik.kuhnke@gmail.com>
2022-12-06 19:51:33 +0100

    aoc 2022 day 6

---
+++ 2022/6/part2.ts
@@ -0,0 +1,22 @@
+const input = await Deno.readTextFile("./input.txt");
+
+function bufferContainsFourDifferentChars(buffer: string[]) {
+    if (buffer.length !== 14) return false;
+
+    return !buffer.map((char, idx, arr) => {
+        return arr.slice(0, idx).includes(char) || arr.slice(idx + 1).includes(char);
+    }).reduce((a, b) => a || b);
+}
+
+const buffer: string[] = [];
+const inputStream = [...input];
+for (let i=0; i<inputStream.length; i++) {
+    buffer.push(inputStream[i]);
+    if (buffer.length > 14) {
+        buffer.splice(0, 1);
+    }
+    if (bufferContainsFourDifferentChars(buffer)) {
+        console.log(i + 1);
+        break;
+    }
+}
