e4ce5225e97e1c8d2e74d11bfd2375e7a5e08926
Dominik Kuhnke <dominik.kuhnke@gmail.com>
2022-12-13 18:25:13 +0100

    aoc 2022 day 12

---
+++ 2022/12/part2.ts
@@ -0,0 +1,54 @@
+const input = await Deno.readTextFile("./input.txt");
+
+function charToHeight(char: string) {
+    const code = char.charCodeAt(0) - 97;
+    if (char === 'S') return 0;
+    if (char === 'E') return 25;
+    return code;
+}
+
+const charMap =  input.split(/\r?\n/).map(line => line.split(''));
+const map = charMap.map(cs => cs.map(c => charToHeight(c)));
+
+const initialStart = charMap.map((row, idx) => [idx, row.indexOf('S')]).reduce((a, b) => a[1] !== -1 ? a : b, [-1,-1]) as [number, number];
+const end = charMap.map((row, idx) => [idx, row.indexOf('E')]).reduce((a, b) => a[1] !== -1 ? a : b, [-1,-1]) as [number, number];
+const otherStarts = charMap.map((row, idx): [number, number] => [idx, row.indexOf('a')]);
+
+const starts = [initialStart, ...otherStarts]
+
+const rows = map.length;
+const cols = map[0].length;
+
+const distances = Array.from(Array(rows), () => new Array(cols).fill(Number.POSITIVE_INFINITY)) as [number, number][];
+distances[end[0]][end[1]] = 0;
+
+const visitedNodes: [number, number][] = [];
+const nodesToCheck = [end];
+
+while (nodesToCheck.length > 0) {
+    const currentNode = nodesToCheck.splice(0, 1)[0];
+    const rowIdx = currentNode[0];
+    const colIdx = currentNode[1];
+    if (visitedNodes.filter(vn => vn[0] === rowIdx && vn[1] === colIdx).length) continue;
+    const currentNodeHeight = map[rowIdx][colIdx];
+    const currentDistance = distances[rowIdx][colIdx];
+
+    const possibleNeighbors = [[-1, 0], [1, 0], [0, -1], [0, 1]];
+    const neighbors = possibleNeighbors
+        .map((pn): [number, number] => [rowIdx + pn[0], colIdx + pn[1]])
+        .filter(([ri, ci]) => 0 <= ri && ri < rows && 0 <= ci && ci < cols)
+        .filter(n => !visitedNodes.filter(vn => vn[0] === n[0] && vn[1] === n[1]).length)
+        .filter(([ri, ci]) => currentNodeHeight - map[ri][ci] <= 1) as [number, number][];
+
+    neighbors.forEach(([ri, ci]) => {
+        const newDistance = (currentDistance + 1);
+        if (newDistance < distances[ri][ci]) {
+            distances[ri][ci] = newDistance;
+        }
+    });
+    visitedNodes.push(currentNode);
+    neighbors.filter(n => !visitedNodes.filter(vn => vn[0] === n[0] && vn[1] === n[1]).length).forEach(n => nodesToCheck.push(n));
+}
+
+const shortestTrail = starts.map(start => distances[start[0]][start[1]]).sort((a, b) => a-b)[0];
+console.log({shortestTrail});
